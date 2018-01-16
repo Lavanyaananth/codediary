7d4dafa68945ce632b8209f09fdbd56968783ade
Lavanyaananth <lavanyaanand.11@gmail.com>
2018-01-17 00:12:47 +0530

    button type changed

--- dummywebapp/index.js
+++ dummywebapp/index.js
@@ -1,11 +1,20 @@
 window.onload = function(){
 	console.log("test");
+
 	function loginAction(){
+		
 		console.log("login ..");
 		var uname = document.getElementById('uname').value;
+		console.log(uname);
 		var pwd = document.getElementById('pwd').value;
-		location.href = "home.html";	
-	}
+		if (uname == "admin" && pwd == "admin"){
+			document.location.href = "home.html";
+		}
+		else{
+			document.write("user name or password you have entered is wrong");
+		}
+			
+	}	
 	var login = document.getElementById('login');
 login.addEventListener('click', loginAction, false);
 }
