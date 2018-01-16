a8c833790bf7f72d309ed28fd1af1763c0f87715
Lavanyaananth <lavanyaanand.11@gmail.com>
2018-01-16 23:58:13 +0530

    login action

--- dummywebapp/index.js
+++ dummywebapp/index.js
@@ -1,1 +1,11 @@
-console.log("test");
+window.onload = function(){
+	console.log("test");
+	function loginAction(){
+		console.log("login ..");
+		var uname = document.getElementById('uname').value;
+		var pwd = document.getElementById('pwd').value;
+		location.href = "home.html";	
+	}
+	var login = document.getElementById('login');
+login.addEventListener('click', loginAction, false);
+}
