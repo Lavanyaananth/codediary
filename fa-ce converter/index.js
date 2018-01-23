be43dd118580a43286a50002687b92599f0faece
Lavanyaananth <lavanyaanand.11@gmail.com>
2018-01-23 21:49:15 +0530

    converter changes

--- fa-ce converter/index.js
+++ fa-ce converter/index.js
@@ -1,5 +1,10 @@
-
+console.log("hello");
 	function temperatureConverter(valNum) {
   valNum = parseFloat(valNum);
   document.getElementById("outputCelcius").innerHTML=(valNum-32)/1.8;
+
+}
+function tempConverter(cel) {
+  cel = parseFloat(cel);
+  document.getElementById("outputFah").innerHTML=(cel*1.8)+32;
 }
