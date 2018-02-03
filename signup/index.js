dbcfaf5ad17930452f608c28c656c51f33f93536
Lavanyaananth <lavanyaanand.11@gmail.com>
2018-02-03 23:20:21 +0530

    adding g_btn

---
+++ signup/index.js
@@ -0,0 +1,12 @@
+onload = function(){
+console.log("helll");
+
+}
+function successnote(){
+	swal({
+  title: "Signing up successful!",
+  text: "Check your inbox",
+  icon: "success",
+  button: "Close"
+})
+}
