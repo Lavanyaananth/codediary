71a64dfd479af498cace6932e0824e033d5f6445
Lavanyaananth <lavanyaanand.11@gmail.com>
2018-02-04 20:07:40 +0530

    home page

--- signup/js/index.js
+++ signup/js/index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+onload = function(){
+console.log("helll");
+
+}
+function successnote(){
+	swal({
+  title: "Signing up successful!",
+  text: "Check your inbox",
+  icon: "success",
+  button: "Close"
+})
+}
