7f38762055294b93348bdd7efeedc20c7746d468
Lavanyaananth <lavanyaanand.11@gmail.com>
2018-02-07 20:33:37 +0530

    trail 2

--- analog-clock/js/index.js
+++ analog-clock/js/index.js
@@ -7,6 +7,7 @@ onload = function(){
                 hourDeg   = now.getHours() / 12 * 360 + now.getMinutes() / 60 * 30,
                 minuteDeg = now.getMinutes() / 60 * 360 + now.getSeconds() / 60 * 6,
                 secondDeg = now.getSeconds() / 60 * 360,
+
                 stylesDeg = [
                     "@-webkit-keyframes rotate-hour{from{transform:rotate(" + hourDeg + "deg);}to{transform:rotate(" + (hourDeg + 360) + "deg);}}",
                     "@-webkit-keyframes rotate-minute{from{transform:rotate(" + minuteDeg + "deg);}to{transform:rotate(" + (minuteDeg + 360) + "deg);}}",
@@ -15,7 +16,6 @@ onload = function(){
                     "@-moz-keyframes rotate-minute{from{transform:rotate(" + minuteDeg + "deg);}to{transform:rotate(" + (minuteDeg + 360) + "deg);}}",
                     "@-moz-keyframes rotate-second{from{transform:rotate(" + secondDeg + "deg);}to{transform:rotate(" + (secondDeg + 360) + "deg);}}"
                 ].join("");
-
             document.getElementById("clock-animations").innerHTML = stylesDeg;
 
         })();
