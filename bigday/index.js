da39ca03774806bcd98f71020672fcebcf6637d8
Lavanyaananth <lavanyaanand.11@gmail.com>
2018-02-19 20:23:59 +0530

    big day sample abs

--- bigday/index.js
+++ bigday/index.js
@@ -1,1 +1,7 @@
 console.log("I'm travelling");
+var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+var firstDate = new Date(2018,02,19);
+var secondDate = new Date(1991,09,11);
+
+var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+console.log(diffDays);
