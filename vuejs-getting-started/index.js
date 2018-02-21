68db2cd9dde412c8f004aa6811c48e762c300f2f
Lavanyaananth <lavanyaanand.11@gmail.com>
2018-02-21 22:09:50 +0530

    reverse function

--- vuejs-getting-started/index.js
+++ vuejs-getting-started/index.js
@@ -1,10 +1,18 @@
 onload = function(){
 	console.log("vue it is ");
+
 var app = new Vue({
   el: '#app',
   data: {
+  	message: 'go to hell',
     condition: true,
     conditionnot: false
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
   }
 })
+
 }
