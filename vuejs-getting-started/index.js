8e9c3a9d13f240c7375d7d1692e82ab8309dca4b
Lavanyaananth <lavanyaanand.11@gmail.com>
2018-02-23 21:47:12 +0530

    todo

--- vuejs-getting-started/index.js
+++ vuejs-getting-started/index.js
@@ -2,17 +2,28 @@ onload = function(){
 	console.log("vue it is ");
 	console.log("dummy commit");
 
-var app = new Vue({
-  el: '#app',
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//   	message: 'go to hell',
+//     condition: true,
+//     conditionnot: false
+//   },
+//   methods: {
+//     reverseMessage: function () {
+//       this.message = this.message.split('').reverse().join('')
+//     }
+//   }
+// })
+
+var app4 = new Vue({
+  el: '#app-4',
   data: {
-  	message: 'go to hell',
-    condition: true,
-    conditionnot: false
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
   }
 })
 
